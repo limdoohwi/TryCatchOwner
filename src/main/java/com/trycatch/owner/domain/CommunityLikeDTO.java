8cdfdc4740b45e8e287420ab370a4b7a87d01656
wanseok2 <kknd9009@naver.com>
2016-07-28 21:21:27 +0900

    20160728 주녁주녁열매먹은 즐겨찾기 50%

---
+++ src/main/java/com/trycatch/owner/domain/CommunityLikeDTO.java
@@ -0,0 +1,36 @@
+package com.trycatch.owner.domain;
+
+public class CommunityLikeDTO {
+	private int community_like_no;
+	private int community_no;
+	private int member_no;
+	private String community_title;
+	
+	
+	public int getCommunity_like_no() {
+		return community_like_no;
+	}
+	public void setCommunity_like_no(int community_like_no) {
+		this.community_like_no = community_like_no;
+	}
+	public int getCommunity_no() {
+		return community_no;
+	}
+	public void setCommunity_no(int community_no) {
+		this.community_no = community_no;
+	}
+	public int getMember_no() {
+		return member_no;
+	}
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+	public String getCommunity_title() {
+		return community_title;
+	}
+	public void setCommunity_title(String community_title) {
+		this.community_title = community_title;
+	}
+
+
+}
